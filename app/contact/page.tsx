90fc6a65b37bd57103146a157ae89a68daea8054
miryowasuke <mwrs5113@gmail.com>
2025-02-13 19:27:18 +0900

    各ページのタイトル英語に変更

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold text-center mb-8">お問い合わせ</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">Contact</h1>
       <form className="space-y-4">
         <div>
           <label
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,7 @@
 export default function Services() {
   return (
     <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold text-center mb-8">サービス</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">Services</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="bg-gray-100 p-4 shadow-md rounded">
           <h2 className="text-xl font-bold mb-2">映像制作</h2>
